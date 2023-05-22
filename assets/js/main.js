a3f7878086af8988663f8c10dfb049d6c7cf8ccc
unknown <jabri.oldd@gmail.com>
2023-05-22 23:46:53 +0100

    toggling sidebar/menu with javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+//Showing/hiding--menu/sidebar
+const navMenu = document.querySelector("#nav-menu");
+const navToggle = document.querySelector("#nav-toggle");
+const navClose = document.querySelector("#nav-close");
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 //Categories Swiper
 var swiperCategories = new Swiper(".categories__container", {
   spaceBetween: 24,
